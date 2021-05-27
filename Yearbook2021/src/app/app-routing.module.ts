5629d65b465b79514f35cc264b921d54b82d6d6f
Shreeprasad Sonar <ShreeprasadSonar@users.noreply.github.com>
2021-05-27 19:44:18 +0530

    Routing for new components

--- Yearbook2021/src/app/app-routing.module.ts
+++ Yearbook2021/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SEComponent } from './se/se.component';
+import { TEComponent } from './te/te.component';
+import { BEComponent } from './be/be.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: 'se', component: SEComponent },
+  { path: 'te', component: TEComponent },
+  { path: 'be', component: BEComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
