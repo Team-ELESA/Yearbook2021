eaec82565bf8646a3dfcde6012f281a470fc374d
Shreeprasad Sonar <ShreeprasadSonar@users.noreply.github.com>
2021-05-27 20:53:29 +0530

    Navbar Footer changes

--- Yearbook2021/src/app/header/header.component.ts
+++ Yearbook2021/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,12 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $('.navTrigger').click(function () {
+      $(this).toggleClass('active');
+      console.log("Clicked menu");
+      $("#mainListDiv").toggleClass("show_list");
+      $("#mainListDiv").fadeIn();
+    });
   }
 
 }
