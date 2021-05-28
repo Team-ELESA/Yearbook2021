0152c85c4b87eee2dcbe65c1dbd024c96b7f584a
Shreeprasad Sonar <ShreeprasadSonar@users.noreply.github.com>
2021-05-28 16:01:55 +0530

    Homepage Cards added with css styles

--- Yearbook2021/src/app/app-routing.module.ts
+++ Yearbook2021/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { SEComponent } from './se/se.component';
 import { TEComponent } from './te/te.component';
 import { BEComponent } from './be/be.component';
-
+import { DevsComponent } from './devs/devs.component';
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'se', component: SEComponent },
   { path: 'te', component: TEComponent },
-  { path: 'be', component: BEComponent }
+  { path: 'be', component: BEComponent },
+  { path: 'devs', component: DevsComponent }
 ];
 
 @NgModule({
---
+++ Yearbook2021/src/app/devs/devs.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-devs',
+  templateUrl: './devs.component.html',
+  styleUrls: ['./devs.component.scss']
+})
+export class DevsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
