5b7b17c859e8e09f19f80fd51fc3a85500252a65
WorkWithSoham <sohamtembe98@gmail.com>
2021-06-19 22:21:29 +0530

    Google Sheet Data Service added

---
+++ Yearbook2021/src/app/GoogleSheetsData/data.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  httpOptions: any;
+  GSheetConst: any = {
+    'students': '',
+    'teachers': ''
+  }
+
+  constructor(private http: HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+  }
+
+  getSheetData(key: string) {
+    const sheetId = this.GSheetConst[key]
+    const url = `https://spreadsheets.google.com/feeds/list/${sheetId}/od6/public/values?alt=json`;
+
+    return this.http.get(url)
+      .pipe(map((res: any) => {
+          const data = res.feed.entry;
+
+          const returnArray: Array<any> = [];
+          if (data && data.length > 0) {
+            data.forEach(entry => {
+              const obj = {};
+              for (const x in entry) {
+                if (x.includes('gsx$') && entry[x].$t) {
+                  obj[x.split('$')[1]] = entry[x]['$t'];
+                }
+              }
+              returnArray.push(obj);
+            });
+          }
+          return returnArray;
+        })
+      );
+  }
+}
