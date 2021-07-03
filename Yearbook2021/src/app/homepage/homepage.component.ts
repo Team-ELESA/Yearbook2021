3bae189336106cd44109df077368c3a2be13e94f
Shreeprasad Sonar <ShreeprasadSonar@users.noreply.github.com>
2021-07-03 14:35:34 +0530

    Teachers data added without photo

--- Yearbook2021/src/app/homepage/homepage.component.ts
+++ Yearbook2021/src/app/homepage/homepage.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../GoogleSheetsData/data.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
 
-  constructor() { }
+export class HomepageComponent implements OnInit {
+  teachers: any[];
+
+  constructor(
+    private dataService: DataService,
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
+    this.dataService.getSheetData('teachers').subscribe(res => {
+       this.teachers = res
+      console.log(this.teachers);
+    })
+  }
+
+  getSanitized(url){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url)
   }
 
 }
