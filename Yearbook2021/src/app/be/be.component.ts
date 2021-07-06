6b0497803e65bbcadac64a872faf7ad8ad8d8188
WorkWithSoham <sohamtembe98@gmail.com>
2021-07-06 13:07:50 +0530

    BE component data added

--- Yearbook2021/src/app/be/be.component.ts
+++ Yearbook2021/src/app/be/be.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../GoogleSheetsData/data.service';
+import { StudentResponse } from '../interfaces';
 
 @Component({
   selector: 'app-be',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BEComponent implements OnInit {
 
-  constructor() { }
+  tempStudents: StudentResponse[] = []
+  BE_students: StudentResponse[] = []
+
+  constructor(
+    private dataService: DataService
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.dataService.getSheetData('students').subscribe((res: StudentResponse[]) => {
+      this.tempStudents = res
+      this.tempStudents.forEach((student, index) => {
+        if (!student.tf && student['yearfortheacademicyear2020-21'] === 'BE') {
+          const ConstUrl = 'https://drive.google.com/thumbnail?id='
+          const id = student.uploadyourphotograph.split('=')[1]
 
+          student.uploadyourphotograph = ConstUrl + id
+
+          this.BE_students.push(student)
+        }
+      });
+      console.log(this.BE_students)
+    })
+  }
 }
