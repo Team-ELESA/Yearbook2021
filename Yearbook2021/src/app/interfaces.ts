7bb6b72182a7b7fa8d6726156cac3963210753dd
WorkWithSoham <sohamtembe98@gmail.com>
2021-07-06 13:08:02 +0530

    Response interface created

--- Yearbook2021/src/app/interfaces.ts
+++ Yearbook2021/src/app/interfaces.ts
@@ -1,10 +1,21 @@
 export interface TeacherResponse {
-    tf: string,
+    tf: string
     timestamp: string
-    firstname: string,
-    lastname: string,
+    firstname: string
+    lastname: string
     'e-mail': string
-    uploadyourphotograph: string,
-    shareyourexperienceswishesadvicegeneralthoughts: string,
+    uploadyourphotograph: string
+    shareyourexperienceswishesadvicegeneralthoughts: string
     kindlyprovideyourfullnamebelowtoprovideyourconsenttoelesatoincludeyourphotoandquoteintheyearbook2021: string
+}
+
+export interface StudentResponse {
+    tf: string
+    describeyourexperiencejourneymemoriesinaquote: string
+    firstname: string
+    'kindlyprovideyourfullnamebelowtoprovideyourconsenttoelesatoincludeyourphotoandquoteintheyearbook2021.': string
+    lastname: string
+    timestamp: string
+    uploadyourphotograph: string
+    'yearfortheacademicyear2020-21': string
 }
